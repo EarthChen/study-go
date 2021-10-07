7b968f3df0bdf09451894d495913cc615d815c7a
earthchen <youwei.chen@duobei.com>
2021-10-07 19:01:07 +0800

    feat(mock): add mock

--- main/main.go
+++ main/main.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"time"
 )
 
 func Sum(numbers []int) (sum int) {
@@ -41,9 +42,54 @@ func MyGreeterHandler(w http.ResponseWriter, r *http.Request) {
 	Greet(w, "world")
 }
 
+type Sleeper interface {
+	Sleep()
+}
+
+type SpySleeper struct {
+	Calls int
+}
+
+type ConfigurableSleeper struct {
+	duration time.Duration
+}
+
+func (o *ConfigurableSleeper) Sleep() {
+	time.Sleep(o.duration)
+}
+
+func (s *SpySleeper) Sleep() {
+	s.Calls++
+}
+
+func Countdown(out io.Writer, sleeper Sleeper) {
+	for i := 3; i > 0; i-- {
+		fmt.Fprintln(out, i)
+		sleeper.Sleep()
+	}
+	fmt.Fprint(out, "Go!")
+}
+
+type CountdownOperationsSpy struct {
+	Calls []string
+}
+
+func (s *CountdownOperationsSpy) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+func (s *CountdownOperationsSpy) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
+}
+
+const write = "write"
+const sleep = "sleep"
+
 func main() {
-	Greet(os.Stdout, "test")
-	http.ListenAndServe(":5000", http.HandlerFunc(MyGreeterHandler))
+	Countdown(os.Stdout, &ConfigurableSleeper{1 * time.Second})
+	//Greet(os.Stdout, "test")
+	//http.ListenAndServe(":5000", http.HandlerFunc(MyGreeterHandler))
 	//println(Sum([]int{3, 9}))
 	//var numbers = SumAll([]int{1, 2}, []int{0, 9}, []int{})
 	//for _, number := range numbers {
